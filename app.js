f94a6ee9a74603eb710e005580550df0172a827e
Bradley011 <140400108+Bradley011@users.noreply.github.com>
2023-07-24 04:34:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("sservice_t9q32sw", "template_aenblex", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
